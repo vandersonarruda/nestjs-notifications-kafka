c115ca3bd2806061996e3760f654ac10ced1f25a
Vanderson Arruda <contato@vanderson.com.br>
2024-09-16 20:27:09 -0300

    add prisma and docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from './http.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
