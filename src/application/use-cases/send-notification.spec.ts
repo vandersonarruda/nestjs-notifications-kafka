72ec5b22ef4c8646b86374a6ad3fd8b87271d1fe
Vanderson Arruda <contato@vanderson.com.br>
2024-09-17 15:53:34 -0300

    feat: add repository contract

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification): Promise<void> {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'Nova solicitação de amizade',
       category: 'social',
       recipientId: 'example-user-id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
