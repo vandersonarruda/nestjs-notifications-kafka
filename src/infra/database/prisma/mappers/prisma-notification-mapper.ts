30fd4ee2e3a5329d305f698c7a6a2cb9c148fc5f
Vanderson Arruda <contato@vanderson.com.br>
2024-09-18 14:55:49 -0300

    feat: implement prisma method and add mapper method to convert to domain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,6 +1,9 @@
+import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
+  // Convert from Domain to Prisma
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -10,5 +13,20 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  // Convert from Prisma to Domain
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
