2b8ec3828a129e255e774f9e9e869a71f91ee9d2
Vanderson Arruda <contato@vanderson.com.br>
2024-09-19 15:17:34 -0300

    chore: send notification received by kafka to send-notification route

--- src/infra/messaging/kafka/controllers/notification.controller.ts
+++ src/infra/messaging/kafka/controllers/notification.controller.ts
@@ -1,11 +1,26 @@
+import { SendNotification } from '@application/use-cases/send-notification';
 import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+interface SendNotificationBody {
+  content: string;
+  category: string;
+  recipientId: string;
+}
 
 @Controller()
 export class NotificationController {
+  constructor(private sendNotification: SendNotification) {}
+
+  // every time a message is received, this method will be called
   @EventPattern('notifications.send-notification')
-  async handleSendNotification() {
-    // every time a message is received, this method will be called
-    console.log('teste KAFKA');
+  async handleSendNotification(
+    @Payload() { content, category, recipientId }: SendNotificationBody,
+  ) {
+    await this.sendNotification.execute({
+      content,
+      category,
+      recipientId,
+    });
   }
 }
